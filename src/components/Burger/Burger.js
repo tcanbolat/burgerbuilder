154954f442134eb84df7affaceba1e1b46a68c8a
atcanbolat <kangalcanbolat@gmail.com>
2020-06-26 20:20:08 -0700

    generating burger ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,16 @@ import classes from './Burger.module.css'
 import Burgeringredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <Burgeringredient key={igKey + i} type={igKey} />;  
+        });
+    });
     return (
         <div className={classes.Burger}>
             <Burgeringredient type="bread-top" />
-            <Burgeringredient type="cheese" />
-            <Burgeringredient type="meat" />
+            {transformedIngredients}
             <Burgeringredient type="bread-bottom" />
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,25 @@ import Aux from '../../hoc/Auxillary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+
+    //     }
+    // }
+state = {
+    ingredinets: {
+        salad: 1,
+        bacon: 1,
+        cheese: 2,
+        meat: 2
+    }
+}
+
     render () {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredinets}/>
                 <div>Build Controls</div>
             </Aux>
         );
