44436f252ecf1d93144937c64d0835a2471b58a9
atcanbolat <kangalcanbolat@gmail.com>
2020-07-09 22:16:41 -0700

    linked orders to firbase to pull orders on screen

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import classes from './NavigationItem.module.css';
+import classes from "./NavigationItem.module.css";
+import { NavLink } from "react-router-dom";
 
 const navigationItem = (props) => (
-    <li className={classes.NavigationItem}>
-        <a 
-            href={props.link} 
-            className={props.active ? classes.active : null}>{props.children}</a>
-    </li>
+  <li className={classes.NavigationItem}>
+    <NavLink exact={props.exact} activeClassName={classes.active} to={props.link}>
+      {props.children}
+    </NavLink>
+  </li>
 );
 
 export default navigationItem;
