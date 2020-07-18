76fc005a5f10140a3f24611d200afe541ee24a4c
atcanbolat <kangalcanbolat@gmail.com>
2020-07-17 18:42:33 -0700

    refactored reducers to make switch case more lean

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -17,14 +17,14 @@ export const purchaseBurgerFail = (error) => {
 };
 
 export const purchaseBurgerStart = () => {
-    return {
-        type: actionTypes.PURCHASE_BURGER_START
-    };
+  return {
+    type: actionTypes.PURCHASE_BURGER_START,
+  };
 };
 
 export const purchaseBurger = (orderData) => {
   return (dispatch) => {
-      dispatch(purchaseBurgerStart());
+    dispatch(purchaseBurgerStart());
     axios
       .post("/orders.json", orderData)
       .then((response) => {
@@ -37,7 +37,45 @@ export const purchaseBurger = (orderData) => {
 };
 
 export const purchaseInit = () => {
-    return {
-        type: actionTypes.PURCHASE_INIT
-    }
+  return {
+    type: actionTypes.PURCHASE_INIT,
+  };
+};
+
+export const fetchOrdersSuccess = (orders) => {
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS,
+    orders: orders,
+  };
+};
+
+export const fetchOrdersFail = (error) => {
+  return {
+    type: actionTypes.FETCH_ORDERS_FAIL,
+    error: error,
+  };
+};
+
+export const fetchOrdersStart = () => {
+  return {
+    type: actionTypes.FETCH_ORDERS_START,
+  };
+};
+
+export const fetchOrders = () => {
+  return (dispatch) => {
+    dispatch(fetchOrdersStart());
+    axios
+      .get("/orders.json")
+      .then((res) => {
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({ ...res.data[key], id: key });
+        }
+        dispatch(fetchOrdersSuccess(fetchedOrders));
+      })
+      .catch((err) => {
+        dispatch(fetchOrdersFail(err));
+      });
+  };
 };
